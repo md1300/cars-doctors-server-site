f41a379319a341da903514373e3ed680c80beffe
md1300 <md.tawhid.khan1998@gmail.com>
2024-10-14 01:53:01 +0600

    search and sort function

---
+++ ..........jwt.....token......js
@@ -0,0 +1,19 @@
+/**
+ * ------------------------
+ * make api secure
+ * ------------------------------
+ * the person who should have
+ * concept:
+ * 1.asign two tokens for each person(access token,refresh token )
+ * 2.access token contains : user identification (email,role,etc).valid for a shorter duration 
+ * 3.refresh token is used : to recreate an access token that was expired.
+ * 4.if refresh is invalid then logout the user.
+ * 
+ * 
+ * 
+ * */ 
+/**
+ * jwt-json web token
+ * 
+ * 
+ * */ 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const cors=require('cors')
-const jwt = require('jsonwebtoken');
-const cookieParser=require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express()
@@ -9,11 +9,17 @@ const port=process.env.PORT || 5000 ;
 
 // midleware---------------
  app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin:[
+    'http://localhost:5173',
+    // 'cars-doctor-f2736.web.app',
+    // 'cars-doctor-f2736.firebaseapp.com',
+  ],
   credentials:true
  }))
  app.use(express.json());
- app.use(cookieParser());
+
+ app.use(cookieParser())
+ 
 
  app.get('/',(req,res)=>{
     res.send('Doctor server is running')
@@ -24,30 +30,30 @@ const port=process.env.PORT || 5000 ;
 // middlesWares -----------------------
 
 const logger=async(req,res,next)=>{
-  console.log('called',req.host,req.originalUrl);
+  console.log(req.method , req.url)
   next()
-};
-
-const veryfyToken=async(req,res,next)=>{
-  const token=req.cookies?.token ;
-  console.log('value of token in middleWare',token)
+}
+const verifyToken = async(req,res,next)=>{
+  const token=req?.cookies?.token ;
+  // console.log('token use in middleware :' ,token)
+  // no token available
   if(!token){
-    return res.status(401).send({message:'not authorized'})
+    return res.status(401).send({message:'unauthorized access'})
   }
- jwt.verify(token,process.env.JWT_ACCESS_TOKEN,(err,decoded)=>{
-  // err
-  if(err){
-    return res.status(401).send({message:'unAuthorized'})
-  }
-  // if token is valid then it would be decoded
-  console.log('value in the token ',decoded)
-  req.user=decoded ;
-  next()
- })
-  
-
+  jwt.verify(token,process.env.JWT_ACCESS_TOKEN,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({message:'unAuthorized'})
+    }
+    else{
+      req.user=decoded;
+      next()
+    }
+  })
   
 }
+
+
+
 
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vmhty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
  
@@ -60,30 +66,50 @@ const veryfyToken=async(req,res,next)=>{
    }
  });
  
+ const cookieOption={
+  httpOnly:true,
+  secure:process.env.NODE_ENV==="prodection"?true:false,
+  sameSite:process.env.NODE_ENV==="production"?"none":"strict"  
+}
+
  async function run() {
    try {
      // Connect the client to the server	(optional starting in v4.7)
-     await client.connect();
+    //  await client.connect();
 
      const serviceCollection=client.db("carsDoctor").collection("services");
 
      const bookingCollection=client.db("carsDoctor").collection("booking")
     //  ---------------Auth related api ----------------
-   app.post('/jwt',logger ,async(req,res)=>{
+  app.post('/jwt',logger, async(req,res)=>{
     const user=req.body;
-    console.log(user)
-    const token=jwt.sign(user,process.env.JWT_ACCESS_TOKEN,{expiresIn:'1h'})
-    res
-    .cookie('token',token,{
-      httpOnly:true,
-      secure:false,
-      
-    })
+    console.log(user);
+    const token =jwt.sign(user,process.env.JWT_ACCESS_TOKEN,{expiresIn:'1h'})
+    res.cookie('token',token,cookieOption)
     .send({success:true})
   })
+
+  app.post('/logOut',async(req,res)=>{
+    const user=req.body;
+    console.log('user log out token:',user)
+    res.clearCookie('token',{cookieOption,maxAge:0}).send({success:true})
+  })
 // -------------------services related api -------------------------
-     app.get('/services',logger, async(req,res)=>{
-        const cursor=serviceCollection.find()
+     app.get('/services', async(req,res)=>{
+      const filter=req.query
+      console.log(filter)
+      // do not work because 150 and 30 are not a number
+      // const query={
+      //  price:{$lt : 150, $gt:30}
+      // }
+      const query={
+       title:{$regex: filter.search, $options:'i'}
+      }
+      const options={
+        sort:{
+          price:filter.sort=== 'asc'? 1:-1}
+      }
+        const cursor=serviceCollection.find(query,options)
         const result=await cursor.toArray()
         res.send(result)
      })
@@ -101,10 +127,14 @@ const veryfyToken=async(req,res,next)=>{
      })
 
     //  ------------- booking operation ------------------
-  app.get('/bookings',logger,veryfyToken, async(req,res)=>{
+  
+  app.get('/bookings',logger,verifyToken, async(req,res)=>{
     console.log(req.query.email)
-    // console.log('tok tok token', req.cookies.token)
+    // console.log('cook cook cookies :',req.cookies)
     console.log("user in the valid token",req.user)
+    if(req.user.email !== req.query.email){
+      return res.status(403).send({message:'forbiden access'})
+    }
     let query={}
     if(req.query.email){
       query={email:req.query.email}
